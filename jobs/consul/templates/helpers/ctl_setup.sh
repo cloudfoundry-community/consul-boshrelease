5995ba3d896e47aee31dc86158cb61bdb5c83d10
James Hunt <james@niftylogic.com>
2016-01-21 17:34:04 -0800

    Support agent-mode and explicit service registration

--- jobs/consul/templates/helpers/ctl_setup.sh
+++ jobs/consul/templates/helpers/ctl_setup.sh
@@ -30,7 +30,7 @@ redirect_output ${output_label}
 export HOME=${HOME:-/home/vcap}
 
 # Add all packages' /bin & /sbin into $PATH
-for package_bin_dir in $(ls -d /var/vcap/packages/*/*bin)
+for package_bin_dir in $(ls -d /var/vcap/packages/*/*bin | grep -v busybox)
 do
   export PATH=${package_bin_dir}:$PATH
 done
